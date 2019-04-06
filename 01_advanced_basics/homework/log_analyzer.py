ee3ad0c073f9d073754e9641f7389be9060ed205
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-04-06 21:42:46 +0700

    Refactor log_analyzer.

--- 01_advanced_basics/homework/log_analyzer.py
+++ 01_advanced_basics/homework/log_analyzer.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import argparse
-import os
-import configparser
 import logging
+import os
 import sys
+
+from collections import namedtuple
+from datetime import datetime
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-
-log_format = '%(filename)s# %(levelname)-8s %(message)s'
-
-logging.basicConfig(format=log_format, level=logging.INFO)
 
 config = {
     "REPORT_SIZE": 1000,
@@ -21,37 +18,73 @@ config = {
     "LOG_DIR": "./log"
 }
 
+LOG_NAME_HEADER = 'nginx-access-ui.log-'
+LNHL = len(LOG_NAME_HEADER)
 
-def parse_cli_args():
-    parser = argparse.ArgumentParser(description='nginx log analyzer.')
-    parser.add_argument('--config', action='store', help='path to config file')
-    return parser.parse_args()
+LogFile = namedtuple('LogFile', ['path', 'name', 'date', 'ext'])
 
 
-def update_config():
-    global config
-    config_path = parse_cli_args().config
-    if config_path:
-        if os.path.isfile(config_path):
-            cfg = configparser.ConfigParser()
-            try:
-                if cfg.read(config_path)[0] != config_path:
-                    logging.error('Error parsing config file')
-                    sys.exit(1)
-                else:
-                    logging.info('Read config file: ' + config_path)
-                    for c in cfg['DEFAULT']:
-                        config[c.upper()] = cfg['DEFAULT'][c]
-            except configparser.Error as err:
-                logging.error('Error parsing config file')
-                logging.error(err)
-                sys.exit(1)
-        else:
-            logging.error('Config file not found')
+def update_config(config, config_path):
+    if not config_path:
+        return False
+    try:
+        f = open(config_path, 'r')
+        for line in f:
+            arr = line.split()
+            if len(arr) < 2:
+                continue
+            config[arr[0]] = arr[1]
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def cli_config_path():
+    if len(sys.argv) < 3:
+        return None
+    if sys.argv[1] == '--config':
+        return sys.argv[2]
+
+
+def get_last_log(log_dir):
+    file_list = os.listdir(log_dir)
+    logs = list(filter(lambda s: s.startswith(LOG_NAME_HEADER), file_list))
+    logs.sort(reverse=True)
+    if logs != []:
+        log_name = logs[0]
+        log_ext = 'gz' if log_name.endswith('.gz') else ''
+        log_date = datetime.strptime(log_name[LNHL:LNHL + 8], '%Y%m%d')
+        return LogFile(log_dir, log_name, log_date, log_ext)
+    else:
+        return None
 
 
 def main():
-    update_config()
+    config_path = cli_config_path()
+    if config_path:
+        if not update_config(config, config_path):
+            print('Error updating config from file')
+            sys.exit(1)
+
+    log_dir = config.get('LOG_DIR', '')
+    if not log_dir or not os.path.isdir(log_dir):
+        print("Logs directory doesn't exists")
+        sys.exit(1)
+
+    log_file = get_last_log(log_dir)
+    if not log_file:
+        print('Log files not found in log directory')
+        sys.exit(1)
+
+    # report_dir = config.get('REPORT_DIR', '')
+    # if not report_dir:
+    #     pass
+    # elif not os.path.isdir(report_dir):
+    #     print("Report directory doesn't exists")
+    #     sys.exit(1)
+
+    # os.listdir()
+    # logging.basicConfig(format='', level=logging.INFO)
 
 
 if __name__ == "__main__":
