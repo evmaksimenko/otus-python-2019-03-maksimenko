b068ff19c0d8b5aefe15fb69d637c2849f9382ae
Eugenii Maksimenko <eugene.maksimenko@gmail.com>
2019-04-07 20:40:48 +0700

    Add report dir checking.

--- 01_advanced_basics/homework/log_analyzer.py
+++ 01_advanced_basics/homework/log_analyzer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import gzip
 import logging
 import os
 import re
@@ -56,10 +57,11 @@ def get_last_log(log_dir):
     logs.sort(reverse=True)
     if logs != []:
         log_name = logs[0]
+        log_path = os.path.join(log_dir, log_name)
         m = LOG_RE.match(log_name)
         log_ext = m.group(2) if m.group(2) else ''
         log_date = datetime.strptime(m.group(1), '%Y%m%d')
-        return LogFile(log_dir, log_name, log_date, log_ext)
+        return LogFile(log_path, log_name, log_date, log_ext)
     else:
         return None
 
@@ -84,11 +86,22 @@ def main():
         sys.exit(0)
 
     report_dir = config.get('REPORT_DIR', '')
-    if report_dir != '' and not os.path.isdir(report_dir):
-        logging.error("Report directory doesn't exists")
-        sys.exit(1)
+    if report_dir != '':
+        if not os.path.isdir(report_dir):
+            logging.error("Report directory doesn't exists")
+            sys.exit(1)
+        log_date = log_file.date
+        report_file = 'report-{:4d}.{:02d}.{:02d}.html'.format(log_date.year, log_date.month, log_date.day)
+        print(report_file)
+        # if report_exists(report_dir)
+
     fname = None if report_dir == '' else report_dir
     logging.basicConfig(format=log_format, level=logging.INFO, filename=fname)
+
+    open_fn = gzip.open if log_file.ext == '.gz' else open
+    with open_fn(log_file.path, 'rt') as log:
+        for line in log:
+            pass
 
 
 if __name__ == "__main__":
